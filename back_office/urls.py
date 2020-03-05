0d7482f7a9a415fcb58ab85e21c4744eb636df8c
Eduardo <ecgb2007@gmail.com>
2020-03-05 10:05:17 -0300

    Agregue una vista mas de prueba y funciona.

--- back_office/urls.py
+++ back_office/urls.py
@@ -3,4 +3,5 @@ from back_office import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("test/", views.test, name="test")
 ]
--- back_office/views.py
+++ back_office/views.py
@@ -4,5 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, Djangoooooooooo! prueba de commit y si funcuina servidor")
+    return HttpResponse("Hello, Djangoooooooooo! prueba de commit y si funciona servidor")
 
+def test(request):
+    return HttpResponse("Esta es una prueba de la pagina test")
