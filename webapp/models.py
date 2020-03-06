81896591d1ffba2e39c1033bcdc2867685375f5c
Leandro Toledo <lgtoledo@outlook.com>
2020-03-06 19:58:45 -0300

    Se crean modelos TipoCombustible,  y se mejora modelo Vehículo

--- webapp/models.py
+++ webapp/models.py
@@ -30,13 +30,21 @@ class Estado(models.Model):
     servicio = models.ManyToManyField(Servicio)
     fecha = models.DateTimeField()
 
-class vehiculo(models.Model):
-    marca = 
-    modelo = 
-    color
+class Vehiculo(models.Model):
+    modelo = models.ForeignKey(Modelo)
+    color = models.CharField(max_length=10)
     nroChasis = models.CharField(max_length=50)
     matricula = models.CharField(max_length=50)
-    anio
-    tipoCombustible
+    anio = models.IntegerField 
+    tipoCombustible =
 
+class Marca(models.Model):
+    nombre = models.CharField(max_length=20)
+
+class Modelo(models.Model):
+    nombre = models.CharField(max_length=20)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE) #Modelo pertenece a una Marca
+
+class TipoCombustible(models.Model):
+    nombre = models.CharField(max_length=20)
 
