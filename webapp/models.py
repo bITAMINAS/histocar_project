db49d9243bd8d95149d96faa15beaaf2343049e4
Leandro Toledo <lgtoledo@outlook.com>
2020-03-06 19:09:10 -0300

    Se agregan atributos de direccion a Usuario, y se crea mod Vehiculo

--- webapp/models.py
+++ webapp/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
-# Create your models here.
 
 class Usuario(models.Model):
-    """Se crea el modelo usuario"""
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
     documento = models.CharField(max_length=8)
     email = models.EmailField(unique=True, default="") #unique=True sirve para que no se repita en la bd
     telefono = models.CharField(max_length=20)
     tipoUsuario = models.IntegerField(default=0)
-    direccion = models.CharField(max_length=200)
+    dirDepartamento = models.CharField(max_length=20)
+    dirCiudad = models.CharField(max_length=20)
+    dirCalle = models.CharField(max_length=50)
+    dirNumero = models.CharField(max_length=5)
     
 class Servicio(models.Model):
-    """"Modelo de servicios"""
-    #id no es necesario colocarse, lo hace solo django
     fecha = models.DateTimeField()
-    texto = models.CharField(max_length=240)
+    textoOtros = models.CharField(max_length=240)
     comentario = models.CharField(max_length=240)
+    kilometros = models.IntegerField 
     puntuacion = models.IntegerField 
     costo = models.IntegerField 
     
@@ -29,3 +29,14 @@ class Estado(models.Model):
     descripcion = models.CharField(max_length=240)
     servicio = models.ManyToManyField(Servicio)
     fecha = models.DateTimeField()
+
+class vehiculo(models.Model):
+    marca = 
+    modelo = 
+    color
+    nroChasis = models.CharField(max_length=50)
+    matricula = models.CharField(max_length=50)
+    anio
+    tipoCombustible
+
+
