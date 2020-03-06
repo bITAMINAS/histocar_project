58749c7a4909346984cf740392ac9aaf5129cb6e
Leandro Toledo <lgtoledo@outlook.com>
2020-03-06 20:46:26 -0300

    Se crean varias relaciones, entre ellas la de Vehiculo-Estado (con atributo fecha)

--- webapp/models.py
+++ webapp/models.py
@@ -13,6 +13,7 @@ class Usuario(models.Model):
     dirCalle = models.CharField(max_length=50)
     dirNumero = models.CharField(max_length=5)
     
+
 class Servicio(models.Model):
     fecha = models.DateTimeField()
     textoOtros = models.CharField(max_length=240)
@@ -20,14 +21,25 @@ class Servicio(models.Model):
     kilometros = models.IntegerField 
     puntuacion = models.IntegerField 
     costo = models.IntegerField 
-    
+    vehiculo = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)
+    tareas = models.ManyToManyField(Tarea)
+    estados = models.ManyToManyField(Estados, through='Estado_Servicio')
+
+
 class Tarea(models.Model):
     nombre = models.CharField(max_length=240)
-    
+
+
 class Estado(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=240)
-    servicio = models.ManyToManyField(Servicio)
+
+
+# Relación N:N entre Estado<>Servicio
+class Estado_Servicio(models.Model):
+    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    fecha = models.DateField()
 
 
 class Vehiculo(models.Model):
@@ -36,19 +48,23 @@ class Vehiculo(models.Model):
     TiposCombustibles = models.TextChoices('Nafta', 'Gasoil', 'Híbrido', 'Eléctrico',
                                             'Hidrógeno', 'GLP')
 
-    modelo = models.ForeignKey(Modelo)
+    modelo = models.ForeignKey(Modelo) # Con el atributo modelo ya es suficiente, ya que a partir de él se puede inferir la Marca
     color = models.CharField(blank=True, choices=Colors.choices, max_length=15)
     nroChasis = models.CharField(max_length=50)
     matricula = models.CharField(max_length=50)
     anio = models.IntegerField 
     tipoCombustible = models.CharField(blank=True, choices=TiposCombustibles.choices, max_length=15)
+    duenio = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
 
 class Marca(models.Model):
     nombre = models.CharField(max_length=20)
 
+
 class Modelo(models.Model):
     nombre = models.CharField(max_length=20)
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE) #Modelo pertenece a una Marca
+
 
 class TipoCombustible(models.Model):
     nombre = models.CharField(max_length=20)
