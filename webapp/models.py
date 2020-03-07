ac7f72d0ad758578d892fa146978722335b79110
Rocío Medina <r.medina@outlook.com>
2020-03-07 01:22:21 -0300

    se agrega el método string al modelo Estado

--- webapp/models.py
+++ webapp/models.py
@@ -33,11 +33,15 @@ class Estado(models.Model):
     nombre = models.CharField(max_length=50, default="")
     descripcion = models.CharField(max_length=240, default="")
 
+    def __str__(self):
+        return self.nombre
+
 # Relación N:N entre Estado<>Servicio
 class Estado_Servicio(models.Model):
     servicio = models.ForeignKey('Servicio', on_delete=models.CASCADE)
     estado = models.ForeignKey('Estado', on_delete=models.CASCADE)
     fecha = models.DateField()
+
 
 class Vehiculo(models.Model):
     TiposCombustibles = models.TextChoices('Combustible', 'Nafta Gasoil Híbrido Eléctrico Hidrógeno GLP')
