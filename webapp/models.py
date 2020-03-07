d1f002a1bf8f2939c727e3a429c6550d2725c91a
todobanners <ecgb2007@gmail.com>
2020-03-06 22:23:47 -0300

    Se agrego valor por defecto a campos charfield, se arreglo errores anteriores.

--- webapp/models.py
+++ webapp/models.py
@@ -1,34 +1,34 @@
 from django.db import models
 
 class Usuario(models.Model):
-    nombre = models.CharField(max_length=20)
-    apellido = models.CharField(max_length=20)
-    documento = models.CharField(max_length=8)
+    nombre = models.CharField(max_length=20, default="")
+    apellido = models.CharField(max_length=20, default="")
+    documento = models.CharField(max_length=8, default="")
     email = models.EmailField(unique=True, default="") #unique=True sirve para que no se repita en la bd
-    telefono = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=20, default="")
     tipoUsuario = models.IntegerField(default=0)
-    dirDepartamento = models.CharField(max_length=20)
-    dirCiudad = models.CharField(max_length=20)
-    dirCalle = models.CharField(max_length=50)
-    dirNumero = models.CharField(max_length=5)
+    dirDepartamento = models.CharField(max_length=20, default="")
+    dirCiudad = models.CharField(max_length=20, default="")
+    dirCalle = models.CharField(max_length=50, default="")
+    dirNumero = models.CharField(max_length=5, default="")
     
 class Servicio(models.Model):
     fecha = models.DateTimeField()
-    textoOtros = models.CharField(max_length=240)
-    comentario = models.CharField(max_length=240)
+    textoOtros = models.CharField(max_length=240, default="")
+    comentario = models.CharField(max_length=240, default="")
     kilometros = models.IntegerField 
     puntuacion = models.IntegerField 
     costo = models.IntegerField 
-    vehiculo = models.ForeignKey('Vehiculo', on_delete=models.CASCADE)
+    vehiculo = models.ForeignKey('Vehiculo', on_delete=models.CASCADE, default='1')
     tareas = models.ManyToManyField('Tarea')
-    estados = models.ManyToManyField('Estados', through='Estado_Servicio')
+    estados = models.ManyToManyField('Estado', through='Estado_Servicio')
 
 class Tarea(models.Model):
-    nombre = models.CharField(max_length=240)
+    nombre = models.CharField(max_length=240, default="")
 
 class Estado(models.Model):
-    nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=240)
+    nombre = models.CharField(max_length=50, default="")
+    descripcion = models.CharField(max_length=240, default="")
 
 # Relación N:N entre Estado<>Servicio
 class Estado_Servicio(models.Model):
@@ -48,18 +48,18 @@ class Vehiculo(models.Model):
     #saludos
     modelo = models.ForeignKey('Modelo', on_delete=models.CASCADE) # Con el atributo modelo ya es suficiente, ya que a partir de él se puede inferir la Marca
     color = models.CharField(blank=True, choices=Colores.choices, max_length=15)
-    nroChasis = models.CharField(max_length=50)
-    matricula = models.CharField(max_length=50)
+    nroChasis = models.CharField(max_length=50, default="")
+    matricula = models.CharField(max_length=50, default="")
     anio = models.IntegerField 
     tipoCombustible = models.CharField(blank=True, choices=TiposCombustibles.choices, max_length=15)
     duenio = models.ForeignKey('Usuario', on_delete=models.CASCADE)
 
 class Marca(models.Model):
-    nombre = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=20, default="")
 
 class Modelo(models.Model):
-    nombre = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=20, default="")
     marca = models.ForeignKey('Marca', on_delete=models.CASCADE) #Modelo pertenece a una Marca
 
 class TipoCombustible(models.Model):
-    nombre = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=20, default="")
