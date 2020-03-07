2d0516cab0dcbc5c4b7691ce368879998a30f3c0
todobanners <ecgb2007@gmail.com>
2020-03-06 22:04:38 -0300

    No putea por los choices, but si por las entidades

--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Usuario(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
@@ -13,7 +12,6 @@ class Usuario(models.Model):
     dirCalle = models.CharField(max_length=50)
     dirNumero = models.CharField(max_length=5)
     
-
 class Servicio(models.Model):
     fecha = models.DateTimeField()
     textoOtros = models.CharField(max_length=240)
@@ -25,46 +23,43 @@ class Servicio(models.Model):
     tareas = models.ManyToManyField('Tarea')
     estados = models.ManyToManyField('Estados', through='Estado_Servicio')
 
-
 class Tarea(models.Model):
     nombre = models.CharField(max_length=240)
-
 
 class Estado(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=240)
 
-
 # Relación N:N entre Estado<>Servicio
 class Estado_Servicio(models.Model):
-    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
-    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    servicio = models.ForeignKey('Servicio', on_delete=models.CASCADE)
+    estado = models.ForeignKey('Estado', on_delete=models.CASCADE)
     fecha = models.DateField()
 
-
 class Vehiculo(models.Model):
-    Colors = models.TextChoices('Blanco', 'Rojo', 'Negro')
-    #, 'Azul', 'Bordó', 'Marrón', 'Gris Plata', 'Gris Ceniza', 'Amarillo', 'Verde', 'Otro')
-    TiposCombustibles = models.TextChoices('Nafta', 'Gasoil', 'Híbrido', 'Eléctrico', 'Hidrógeno', 'GLP')
-
-    modelo = models.ForeignKey(Modelo) # Con el atributo modelo ya es suficiente, ya que a partir de él se puede inferir la Marca
-    color = models.CharField(blank=True, choices=Colors.choices, max_length=15)
+    TiposCombustibles = models.TextChoices('Combustibles', 'Nafta Gasoil Híbrido Eléctrico Hidrógeno GLP')
+    Colores = models.TextChoices('Color', 'Blanco Rojo Negro Azul Bordó Marrón Gris_Plata Gris_Ceniza Amarillo Verde Otro')
+    #Para probar que esta opcion funciona: Abre una consola y pone
+    #python
+    #from django.db import models
+    #Colores = models.TextChoices('Color', 'Blanco Rojo Negro Azul Bordó Marrón Gris_Plata')
+    #Colores.choices
+    # Deberia devovler: [('Blanco', 'Blanco'), ('Rojo', 'Rojo'), ('Negro', 'Negro'), ('Azul', 'Azul'), ('Bordó', 'Bordó'), ('Marrón', 'Marrón'), ('Gris_Plata', 'Gris Plata')]
+    #saludos
+    modelo = models.ForeignKey('Modelo', on_delete=models.CASCADE) # Con el atributo modelo ya es suficiente, ya que a partir de él se puede inferir la Marca
+    color = models.CharField(blank=True, choices=Colores.choices, max_length=15)
     nroChasis = models.CharField(max_length=50)
     matricula = models.CharField(max_length=50)
     anio = models.IntegerField 
     tipoCombustible = models.CharField(blank=True, choices=TiposCombustibles.choices, max_length=15)
     duenio = models.ForeignKey('Usuario', on_delete=models.CASCADE)
 
-
 class Marca(models.Model):
     nombre = models.CharField(max_length=20)
-
 
 class Modelo(models.Model):
     nombre = models.CharField(max_length=20)
     marca = models.ForeignKey('Marca', on_delete=models.CASCADE) #Modelo pertenece a una Marca
 
-
 class TipoCombustible(models.Model):
     nombre = models.CharField(max_length=20)
-
