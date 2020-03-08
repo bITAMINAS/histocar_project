05361c4bd1f8b4e88ce24602879dfd10fe90c465
todobanners <ecgb2007@gmail.com>
2020-03-08 03:32:34 -0300

    nuevas urls para los modulos

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,4 +4,6 @@ from webapp import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("index.html", views.index, name="index"),
+    path('ver-servicios', views.verServicios, name='VerServicios'),
+    path('crear-servicio', views.crearServicio, name='CrearServicio'),
 ]
