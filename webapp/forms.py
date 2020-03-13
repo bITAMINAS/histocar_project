e8156e66211c9fff0d689a8eff9d722722d64d3d
Leandro Toledo <lgtoledo@outlook.com>
2020-03-13 14:16:29 -0300

    Pendiente de seguir mejorando el form Crear Servicio

--- webapp/forms.py
+++ webapp/forms.py
@@ -2,12 +2,33 @@ from django import forms
 from webapp.models import Servicio, Usuario
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import ReadOnlyPasswordHashField, UserCreationForm
+from django.forms import ModelForm, Select
 
 class ServicioForm(forms.ModelForm):
 
     class Meta:
         model = Servicio
         fields = ('fecha', 'textoOtros', 'kilometros', 'costo', 'vehiculo',  'tareas',  'estados')
+        widgets = {
+            'estados': Select() label='Estado',
+        }
+
+
+# class CrearServicioForm(forms.Form):
+#     name = forms.CharField(
+#         label='Nombre',
+#     )
+
+#     email = forms.EmailField(
+#         label='Correo electrónico',
+#     )
+
+#     message = forms.CharField(
+#         label='Mensaje',
+#         widget=forms.Textarea,
+#     )
+
+
 
 class registroUsuario(UserCreationForm):
     class Meta:
