db31941e6d5092f3f445d911e215efde6dec857d
Leandro Toledo <lgtoledo@outlook.com>
2020-03-14 12:47:05 -0300

    Sincronizo cambios de Chasky

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('ver-servicios', views.verServicios, name='VerServicios'),
     path('crear-servicio', views.crearServicio, name='CrearServicio'),
     path('detalles-servicio/<int:servicio_id>', views.detallesServicio, name='DetallesServicio'),
+    path('registro-usuario', views.crearUsuario, name='CrearUsuario'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.utils import timezone
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 
 #Cargamos vistas de los modelos
-from .models import Servicio
-from .forms import ServicioForm
+from .models import Servicio, Usuario
+from .forms import ServicioForm, registroUsuario, Login
 
 # Create your views here.
 
@@ -34,3 +35,35 @@ def detallesServicio(request, servicio_id):
     servicio = Servicio.objects.get(pk=servicio_id)
     seccion = 'Detalles de Servicio'
     return render(request, 'webapp/servicios-detalle.html', {'servicio': servicio, 'seccion': seccion})
+
+def crearUsuario(request):
+    seccion = 'Alta de nuevo Usuario'
+    if request.method == 'POST':
+        form = registroUsuario(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = registroUsuario()
+    return render(request, 'webapp/crear_usuario.html', {'form': form, 'seccion':seccion})
+
+def login(request):
+    seccion= 'Ingreso de usuario'
+    if request.method == 'POST':
+        form = Login(data = request.POST)
+        if form.is_valid():
+            documento = request.POST['documento']
+            password = request.POST['password']
+            user = authenticate(documento=documento, password=password)
+            if user is not None:
+                if user.is_active:
+                    django_login(request,user)
+                    return redirect('/') 
+    else:
+        form = Login()
+
+    return render(request,'webapp/login.html',{'form':form, 'seccion': seccion})
+
+def logout(request):
+    django_logout(request)
+    return redirect('/')
