63fb07a8454793696c3fe0051811c4de52c501b8
Rocío Medina <r.medina@outlook.com>
2020-03-17 23:02:34 -0300

    bd

--- histocar/urls.py
+++ histocar/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("webapp.urls")),
+    
 ]
+
+
