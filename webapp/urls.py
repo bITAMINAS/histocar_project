c567ff00dc0db8b1a53bc14dc53061530f9ca9ba
Leandro Toledo <lgtoledo@outlook.com>
2020-04-03 21:58:03 -0300

    Se ordenan las URLs

--- webapp/urls.py
+++ webapp/urls.py
@@ -11,26 +11,30 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # Servicios
     path('servicios', views.serviciosView, name='Servicios'),
     path('crear-servicio', views.crearServicio, name='CrearServicio'),
     path('servicio/<int:servicio_id>', views.servicioView, name='Servicio'),
     path('modificar-servicio/<int:servicio_id>', views.editarServicio, name='ModificarServicio'),
-    path('registro-usuario', views.crearUsuario, name='CrearUsuario'),
-    path('crear-vehiculo', views.crearVehiculo, name='CrearVehiculo'),
+    # Usuarios
     path('login', views.login, name='signin'),#el name se cambia de login a signin para que no de error en las vistas de recuperar password.
     path('logout', views.logout, name='logout'),
+    path('registro-usuario', views.crearUsuario, name='CrearUsuario'),
     path('listar-usuarios', views.verUsuarios, name='ListarUsuarios'),
     path('usuario-baja/<int:usuario_id>', views.bajaUsuario, name='UsuarioBaja'),
     path('registration/', include('django.contrib.auth.urls')),
     path('detalles-usuario/<int:usuario_id>', views.detallesUsuario, name='DetallesUsuario'),
+    path('editar-usuario/<int:pk>', views.editarUsuario, name='EditarUsuario'),
+    # Vehículos
     path('ver-vehiculos', views.verVehiculosCliente, name='VerVehiculos'),
+    path('crear-vehiculo', views.crearVehiculo, name='CrearVehiculo'),
     path('vehiculo-baja/<int:vehiculo_id>', views.borrarVehiculoCliente, name="BorrarVehiculoCliente"),
     # Clientes
     path('clientes', views.clientesView, name='Clientes'),
     path('cliente', views.clienteView, name='Cliente'),
     path('cliente-crear', views.clienteCrearView, name='ClienteCrear'),
 
-    path('editar-usuario/<int:pk>', views.editarUsuario, name='EditarUsuario'),
+    
     
    
 ]
